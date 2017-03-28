2cbf71a55f01913d75da80daa27dbbd6f64d90d6
Sasha Dunbar <dunbarsa@protonmail.com>
2017-03-28 17:26:17 -0600

    add captures inputs

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// practice printing inputs to console
+$('#add').on('click', function () {
+  var $inputTitle = $('#input-title').val()
+  console.log($inputTitle);
+  var $inputCaption = $('#input-caption').val();
+  console.log($inputCaption);
+});
