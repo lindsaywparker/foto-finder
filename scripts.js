ab81177ff09f7221e4bc931418f9dc6bf66ac3c6
Sasha Dunbar <dunbarsa@protonmail.com>
2017-03-28 17:43:53 -0600

    Playing with Photo Src

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
 // practice printing inputs to console
 $('#add').on('click', function () {
-  var $inputTitle = $('#input-title').val()
+  var $inputTitle = $('#input-title').val();
   console.log($inputTitle);
   var $inputCaption = $('#input-caption').val();
   console.log($inputCaption);
+  // var $inputFileName = $('#choose').val();
+  // console.log($inputFileName);
+  $('.card-photo').attr('src',$('#choose').val());
 });
+
+// practice capturing input type file
