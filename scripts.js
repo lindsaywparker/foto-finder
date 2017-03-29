a91513f0e40ac801048cb301f3fb92e7296ac520
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-03-28 21:01:32 -0600

    Update div placeholders for card icons, small formatting.  Include title and caption on card.

--- scripts.js
+++ scripts.js
@@ -29,12 +29,12 @@ $('#add').click(function() {
   var $inputCaption = $('#input-caption').val();
   // var $filePath = ....
   $('.cards').prepend("<div class='single-card'> \
-  <h2 class='card-title'>" + $inputTitle + "</h2> \
-  <img src='photos/fireworks.jpg' class='card-photo'/> \
-  <p class='card-caption'>" + $inputCaption + "</p> \
-  <p class='card-footer'> \
-  <img src='images/delete.svg' alt='trash can icon- delete' class='card-icon' id='trash'> \
-  <img src='images/favorite.svg' alt='heart icon- save as a favorite' class='card-icon' id='heart'> \
-  </p> \
+    <h2 class='card-title'>" + $inputTitle + "</h2> \
+    <img src='photos/fireworks.jpg' class='card-photo'/> \
+    <p class='card-caption'>" + $inputCaption + "</p> \
+    <p class='card-footer'> \
+      <div id='trash'></div> \
+      <div id='heart'></div> \
+    </p> \
   </div>" )
 });
