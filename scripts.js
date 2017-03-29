af81dca1df7ab23b634182c33075b538035b6830
Sasha Dunbar <dunbarsa@protonmail.com>
2017-03-28 20:23:44 -0600

    add image functionality

--- scripts.js
+++ scripts.js
@@ -1,12 +1,17 @@
+$(document).ready(function(){
+      $('input[type="file"]').change(function(e){
+          var fileName = e.target.files[0].name;
+          console.log('The file "' + fileName +  '" has been selected.');
+      });
+  });
+
+
 // practice printing inputs to console
 $('#add').on('click', function () {
   var $inputTitle = $('#input-title').val();
   console.log($inputTitle);
   var $inputCaption = $('#input-caption').val();
   console.log($inputCaption);
-  // var $inputFileName = $('#choose').val();
-  // console.log($inputFileName);
-  $('.card-photo').attr('src',$('#choose').val());
 });
 
 // practice capturing input type file
