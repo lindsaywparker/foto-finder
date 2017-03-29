b0dddd5e6c601c7127f0bb8c1187d11e1bfd28fb
Sasha Dunbar <dunbarsa@protonmail.com>
2017-03-29 15:12:06 -0600

    add trash removal

--- scripts.js
+++ scripts.js
@@ -1,21 +1,18 @@
 $('#add').click(function() {
-  var $inputTitle = $('#input-title').val();
-  var $inputCaption = $('#input-caption').val();
-  var $filename = $('#choose').val().replace(/C:\\fakepath\\/i, 'photos/');
-  $('.cards').prepend("<div class='single-card'> \
+    var $inputTitle = $('#input-title').val();
+    var $inputCaption = $('#input-caption').val();
+    var $filename = $('#choose').val().replace(/C:\\fakepath\\/i, 'photos/');
+    $('.cards').prepend("<div class='single-card'> \
     <h2 class='card-title'>" + $inputTitle + "</h2> \
     <img src='" + $filename + "' class='card-photo'/> \
     <p class='card-caption'>" + $inputCaption + "</p> \
     <div class='card-footer'> \
-      <div class='card-icons' id='trash'></div> \
-      <div class='card-icons' id='heart'></div> \
+    <div class='card-icons' id='trash'></div> \
+    <div class='card-icons' id='heart'></div> \
     </div> \
-  </div>" )
-});
+    </div>");
 
-function trashIt (event) {
-  var target = $(event.target);
-  if (target.is('#trash')) {
-    $('.single-card').remove();
-  }
-}
+    $('#trash').click(function() {
+        $(this).parent().parent().remove();
+    });
+});
