984f8930be270e879324230e921cc6879d939c52
Sasha Dunbar <dunbarsa@protonmail.com>
2017-03-30 09:59:18 -0600

    tidy and beautify all documents

--- scripts.js
+++ scripts.js
@@ -1,12 +1,30 @@
+function checkContents() {
+  if ($('#input-title').val() !== "" && $('#input-caption').val() !== "" && $('#choose').val() !== "") {
+    $('#add').prop('disabled', false);
+  }
+}
+
+$('#input-title').keyup(function () {
+  checkContents();
+});
+
+$('#input-caption').keyup(function () {
+  checkContents();
+});
+
+$('#choose').change(function () {
+  checkContents();
+});
+
 $('#add').click(function() {
-    var $inputTitle = $('#input-title').val();
-    var $inputCaption = $('#input-caption').val();
-    var $filename = $('#choose').val().replace(/C:\\fakepath\\/i, 'photos/');
+    var inputTitle = $('#input-title').val();
+    var inputCaption = $('#input-caption').val();
+    var filename = $('#choose').val().replace(/C:\\fakepath\\/i, 'photos/');
 
     $('.cards').prepend("<div class='single-card'> \
-      <h2 class='card-title'>" + $inputTitle + "</h2> \
-      <img src='" + $filename + "' class='card-photo'/> \
-      <p class='card-caption'>" + $inputCaption + "</p> \
+      <h2 class='card-title'>" + inputTitle + "</h2> \
+      <img src='" + filename + "' class='card-photo'/> \
+      <p class='card-caption'>" + inputCaption + "</p> \
       <div class='card-footer'> \
       <div class='card-icons' id='trash'></div> \
       <div class='card-icons heart'></div> \
@@ -26,47 +44,4 @@ $('#add').click(function() {
     $('#input-caption').val('');
     $('#choose').val('');
     $('#add').prop('disabled', true);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function checkContents() {
-  if ($('#input-title').val() !== "" && $('#input-caption').val() !== "" && $('#choose').val() !== "") {
-    $('#add').prop('disabled', false);
-  }
-}
-
-$('#input-title').keyup(function () {
-  checkContents();
-});
-
-$('#input-caption').keyup(function () {
-  checkContents();
-});
-
-$('#choose').change(function () {
-  checkContents();
 });
